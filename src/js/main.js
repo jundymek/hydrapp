7f0e772fce7ee4f3ea9ea5833074d6383ceec3c9
jundymek <jundymek@gmail.com>
2019-07-17 01:30:18 +0200

    feat: added classes for water transition

--- src/js/main.js
+++ src/js/main.js
@@ -20,10 +20,12 @@ const add = document.querySelector('.add--js')
 const subtract = document.querySelector('.subtract--js')
 const number = document.querySelector('.glass__number--js')
 const key = new Date().toISOString().slice(0, 10)
+const glass = document.querySelector('.glass__path--js')
 
 if (!localStorage.getItem(key)) {
   localStorage.setItem(key, 0)
   number.innerHTML = 0
+  glass.classList.add('glass__path--0')
 } else {
   number.innerHTML = localStorage.getItem(key)
 }
@@ -32,6 +34,17 @@ add.addEventListener('click', (e) => {
   if (parseInt(number.innerHTML) < 9) {
     number.innerHTML = parseInt(number.innerHTML) + 1
     localStorage.setItem(key, number.innerHTML)
+    glass.classList.remove(`glass__path--${parseInt(number.innerHTML)-1}`)
+    glass.classList.add(`glass__path--${parseInt(number.innerHTML)}`)
+    // switch (parseInt(number.innerHTML)) {
+  
+    //   case 0:
+    //     glass.classList.add('glass__path--0');
+    //     break;
+    //   case 1:
+    //       glass.classList.add('glass__path--1')
+    //       break;
+    // }
   }
 })
 
@@ -39,5 +52,18 @@ subtract.addEventListener('click', (e) => {
   if (parseInt(number.innerHTML) > 0) {
     number.innerHTML = parseInt(number.innerHTML) - 1
     localStorage.setItem(key, number.innerHTML)
+    glass.classList.remove(`glass__path--${parseInt(number.innerHTML)+1}`)
+    glass.classList.add(`glass__path--${parseInt(number.innerHTML)}`)
   }
 })
+
+console.log(parseInt(number.innerHTML))
+switch (parseInt(number.innerHTML)) {
+  
+  case 0:
+    glass.classList.add('glass__path--0');
+    break;
+  case 1:
+      glass.classList.add('glass__path--1')
+      break;
+}
